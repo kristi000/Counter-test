fe4d37d6e2c7d31a93730dc690324bf3ae3b6f1a
kristish000 <kristish000@gmail.com>
2023-04-06 23:01:02 +0200

    barebones couner day 1

---
+++ script.js
@@ -0,0 +1,14 @@
+let add = document.querySelector("#add");
+let subract = document.querySelector("#subtract");
+let number = document.querySelector("#counter");
+let counter = 0;
+
+add.addEventListener("click", () => {
+  counter++;
+  number.textContent = counter;
+});
+
+subract.addEventListener("click", () => {
+  counter--;
+  number.textContent = counter;
+});
